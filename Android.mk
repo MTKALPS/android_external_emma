c7fe3aca878bfe7335aab88e74d8d1063554c119
jmpfbmx <jmpf.bmx@gmail.com>
2022-07-18 21:01:35 +0200

    emma: Import MediaTek changes
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I8ddfe5b016d0241fcb2127804fdc6532b8076148

--- Android.mk
+++ Android.mk
@@ -19,6 +19,21 @@ LOCAL_SDK_VERSION := 8
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
+ifeq ($(strip $(BUILD_MTK_API_DEP)), yes)
+# emma API table.
+# ============================================================
+LOCAL_MODULE := emma-api
+
+LOCAL_JAVA_LIBRARIES += $(LOCAL_STATIC_JAVA_LIBRARIES)
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+
+LOCAL_DROIDDOC_OPTIONS:= \
+		-api $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/emma-api.txt \
+		-nodocs \
+		-hidden
+
+include $(BUILD_DROIDDOC)
+endif
 
 
 # the custom emma library to add to an SDK project.
